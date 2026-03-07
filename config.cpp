c273515c396ee175c9a88ba1075a357e0124a63f
maks77000 <fedorenko367@gmail.com>
2026-03-07 11:02:47 +0300

    Create config.cpp

---
+++ config.cpp
@@ -0,0 +1,41 @@
+class CfgPatches
+{
+	class RadialReloadV4
+	{
+		requiredAddons[] = {"DZ_Data","DZ_Scripts"};
+	};
+};
+
+class CfgMods
+{
+	class RadialReloadV4
+	{
+		type = "mod";
+
+		class defs
+		{
+			class gameScriptModule
+			{
+				files[] = {"RadialReloadV4/scripts/3_Game"};
+			};
+
+			class worldScriptModule
+			{
+				files[] = {"RadialReloadV4/scripts/4_World"};
+			};
+
+			class missionScriptModule
+			{
+				files[] = {"RadialReloadV4/scripts/5_Mission"};
+			};
+		};
+	};
+};
+
+class CfgUserActions
+{
+	class UAReloadRadial
+	{
+		displayName = "Radial Reload";
+	};
+};
